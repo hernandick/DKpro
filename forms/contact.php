89106bdecbe67bd60c94ce0ef4cb4a8e754b37e8
hernandick <hdick@itba.edu.ar>
2022-10-03 13:30:21 -0300

    l

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'dkmailpage@gmail.com',
-    'password' => 'boca1234',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'smtp.gmail.com',
+  //   'username' => 'dkmailpage@gmail.com',
+  //   'password' => 'boca1234',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
